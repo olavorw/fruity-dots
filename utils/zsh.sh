8bf33be291930a531c54686a8ae055adcd9b5830
Olav <olav@olavorw.com>
2025-12-08 19:25:35 -0800

    util

--- utils/zsh.sh
+++ utils/zsh.sh
@@ -10,10 +10,7 @@ PLUGIN_DIR="$HOME/.zsh"
 # Create plugin directory
 mkdir -p "$PLUGIN_DIR"
 
-git clone "https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-git clone "https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-
 echo "Plugins cloned to $PLUGIN_DIR"
-echo "Add to your .zshrc:"
-echo "source $PLUGIN_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh"
-echo "source $PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+
+git clone "https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone "https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
