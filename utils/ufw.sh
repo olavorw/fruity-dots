07a369864933cf292155b04e8994bb5cbd000804
Olav <olav@olavorw.com>
2025-12-11 13:38:17 -0800

    no mow

--- utils/ufw.sh
+++ utils/ufw.sh
@@ -21,7 +21,7 @@ sudo ufw default allow outgoing
 
 # Allow SSH only from Tailscale network (100.64.0.0/10)
 echo "Allowing SSH on port 22 from Tailscale network..."
-sudo ufw allow in on tailscale0 to any port 22
+sudo ufw allow in on tailscale0 #to any port 22
 
 # Enable UFW
 echo "Enabling UFW..."
